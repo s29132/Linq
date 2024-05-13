67f60b2e93a3ddb728e17ba22901917736e8efc4
s29132 <162105080+s29132@users.noreply.github.com>
2024-05-13 10:00:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LinqTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = LinqTasks.Task1();
+
+        }
+        
+        
+    }
+}
